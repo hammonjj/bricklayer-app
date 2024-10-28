203b2a516c97fd47da464333d87e6bec0cd7d2c6
James Hammond <james@thegruppetto.com>
2024-10-27 23:44:45 -0600

    Update auth_repository.dart

--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:bricklayer/core/enums.dart';
 import 'package:bricklayer/core/utils/guid.dart';
 import 'package:bricklayer/repositories/dtos/auth_dto.dart';
 import 'package:bricklayer/repositories/models/user_login_model.dart';
@@ -9,12 +10,18 @@ import 'models/user_registration_model.dart';
 
 class AuthRepository {
   final ApiClient apiClient;
-  final _controller = StreamController<AuthenticationStatus>();
+  final _controller = StreamController<AuthDto?>();
 
   AuthRepository({required this.apiClient});
 
-  Future<AuthDto?> signUp(
-      {required String email, required String password}) async {
+  void dispose() => _controller.close();
+
+  Stream<AuthDto?> get status async* {
+    yield null;
+    yield* _controller.stream;
+  }
+
+  Future<void> signUp({required String email, required String password}) async {
     try {
       final response = await apiClient.post(
         '/signup',
@@ -25,12 +32,14 @@ class AuthRepository {
       if (response.statusCode == 200) {
         final user = UserRegistrationModel.fromJson(response.data);
 
-        return AuthDto(
+        final auth = AuthDto(
           userId: Guid.parse(user.userId),
           username: user.username,
           accessToken: user.accessToken,
           refreshToken: user.refreshToken,
         );
+
+        _controller.add(auth);
       } else {
         throw Exception(response.data['error'] ?? 'Signup failed');
       }
@@ -39,8 +48,7 @@ class AuthRepository {
     }
   }
 
-  Future<AuthDto?> login(
-      {required String email, required String password}) async {
+  Future<void> login({required String email, required String password}) async {
     try {
       final response = await apiClient.post(
         '/login',
@@ -51,12 +59,14 @@ class AuthRepository {
       if (response.statusCode == 200) {
         final user = UserLoginModel.fromJson(response.data);
 
-        return AuthDto(
+        final auth = AuthDto(
           userId: Guid.parse(user.userId),
           username: email,
           accessToken: user.accessToken,
           refreshToken: user.refreshToken,
         );
+
+        _controller.add(auth);
       } else {
         throw Exception(response.data['error'] ?? 'Login failed');
       }
